81e748fa2818c1ffed3408caa48e536a90a8d32f
Desale1 <dkdesale08@gmail.com>
2023-02-06 18:41:36 +0530

    Create main class for address book

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -3,5 +3,6 @@ package com.bridgelabz.Addressbook;
 public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book Program");
+        AddressBook addressBook = new AddressBook();
     }
 }
